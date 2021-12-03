477fb16ea9bc4c95b6b84f1558eafcf946dedfa5
aac-devs <aac.devs@gmail.com>
2021-12-02 21:59:59 -0300

    first commit

---
+++ ts/app/index.ts
@@ -0,0 +1,8 @@
+// Primary file for the API
+
+// Dependencies
+import http from 'http';
+
+// The server should respond to all requests with a string
+const server = http.createServer();
+// Start the server, and have it listening on port 3000
